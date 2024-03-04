8dbfa25d38d60ecaeea2a31e2b35894a86b4e8ea
Coien-rr <972054003@qq.com>
2024-03-04 20:57:20 +0800

    first commit

--- myKeyBoard/AppDelegate.swift
+++ myKeyBoard/AppDelegate.swift
@@ -6,13 +6,32 @@
 //
 
 import Cocoa
+import Foundation
+import IOKit
+import IOKit.usb
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
+    @IBOutlet weak var Menu: NSMenu!
+    var statusItem: NSStatusItem?
+    @IBOutlet weak var ICON: NSMenu!
     
-
-
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        
+        
+        let itemImage = NSImage(named: "Icon")
+        itemImage?.isTemplate = true
+        statusItem?.button?.image = itemImage
+        statusItem?.menu = Menu
+        // 使用这个函数来获取USB设备列表
+        let usbDeviceNames = getConnectedUSBDevices()
+        statusItem?.button?.title = usbDeviceNames[0]
+    }
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
     }
@@ -21,10 +40,47 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // Insert code here to tear down your application
     }
 
-    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
-        return true
+    @IBAction func Update(_ sender: Any) {
+        print("Todo Update")
     }
+    
+    @IBAction func Quit(_ sender: Any) {
+        NSApplication.shared.terminate(self)
+    }
+    
+    func getConnectedUSBDevices() -> [String] {
+        var productNames: [String] = []
+        let masterPort = UnsafeMutablePointer<mach_port_t>.allocate(capacity: 1)
+        guard IOMasterPort(mach_port_t(MACH_PORT_NULL), masterPort) == KERN_SUCCESS else {
+            print("Failed to create a master I/O Kit port")
+            return productNames
+        }
 
+        let matchingDict = IOServiceMatching(kIOUSBDeviceClassName) as NSMutableDictionary
+        let iterator = UnsafeMutablePointer<io_iterator_t>.allocate(capacity: 1)
+        guard IOServiceGetMatchingServices(masterPort.pointee, matchingDict, iterator) == KERN_SUCCESS else {
+            print("Failed to get matching services")
+            return productNames
+        }
 
+        var service: io_object_t
+        repeat {
+            service = IOIteratorNext(iterator.pointee)
+            if service != 0 {
+                let serviceDict = UnsafeMutablePointer<Unmanaged<CFMutableDictionary>?>.allocate(capacity: 1)
+                if IORegistryEntryCreateCFProperties(service, serviceDict, kCFAllocatorDefault, 0) == KERN_SUCCESS {
+                    if let dict = serviceDict.pointee?.takeRetainedValue() as? [String: Any],
+                                       let productName = dict["USB Product Name"] as? String,
+                                       !productName.contains("Hub") {
+                                        productNames.append(productName)
+                                    }
+                                }
+                IOObjectRelease(service)
+            }
+        } while service != 0
+
+        IOObjectRelease(iterator.pointee)
+        return productNames
+    }
 }
 
